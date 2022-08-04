ce2283baf8f27a141a1efd375a6e19cf6d4b4651
abiodunosagie <abiodunosagie8@gmail.com>
2022-08-04 12:42:42 +0100

    i changed the 180 number to 150

--- lib/Screens/input_Page.dart
+++ lib/Screens/input_Page.dart
@@ -19,7 +19,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender? selectedGender;
-  int height = 180;
+  int height = 150;
   int weight = 60;
   int age = 27;
 
